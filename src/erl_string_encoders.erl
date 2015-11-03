4d4bcf20d44d693de4a2fd3404162e22a9fee519
Louis-Philippe Gauthier <lpgauth@gmail.com>
2015-11-02 23:39:49 -0500

    Fix dialyzer warnings

--- src/erl_string_encoders.erl
+++ src/erl_string_encoders.erl
@@ -29,8 +29,11 @@
     double_to_string_variable/2
 ]).
 
+-define(NOT_LOADED, not_loaded(?LINE)).
+
 -on_load(init/0).
 
+%% public
 init() ->
     PrivDir = case code:priv_dir(?MODULE) of
                   {error, _} ->
@@ -47,70 +50,74 @@ init() ->
     end.
 
 base64_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base64_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 web_safe_base64_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 web_safe_base64_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base85_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base85_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base16_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base16_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base2_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 base2_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 url_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 url_min_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 url_decode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 javascript_encode(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 int_to_string(_Int) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 int64_to_string(_Int64) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 uint_to_string(_UInt) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 uint64_to_string(_UInt64) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 double_to_string_fixed(_Double, _Precision) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 double_to_string_variable(_Double, _Precision) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 to_upper(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 to_lower(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
 
 to_print(_Binary) ->
-    {error, erl_string_encoders_nif_not_loaded}.
+    ?NOT_LOADED.
+
+%% private
+not_loaded(Line) ->
+    erlang:nif_error({not_loaded, [{module, ?MODULE}, {line, Line}]}).
